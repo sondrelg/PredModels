a9e0f875680111fce9469b1dfbcd6c6bc09c5d64
Sondro <s>
2019-03-23 16:18:41 +0100

    Added ticker specific stock histories to api

--- pm/api/urls.py
+++ pm/api/urls.py
@@ -1,6 +1,6 @@
 from api.views.json import ViewRequest
 from api.views.tickers import ListTickers
-
+from api.views.history import ListHistory
 
 
 from django.urls import path, include
@@ -11,6 +11,6 @@ urlpatterns = [
 
     # Json paster
     path('json/', ViewRequest.as_view(), name='ViewRequest'),
-
-    path('tickers/', ListTickers.as_view(), name='ViewRequest'),
+    path('data/tickers', ListTickers.as_view(), name='ViewRequest'),
+    path('data/<ticker>', ListHistory.as_view(), name='ListHistory')
     ]
