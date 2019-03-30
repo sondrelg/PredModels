2bd627392712eb10b5d0115b9ea5de849a11ce34
unknown <sondrelg@live.no>
2019-03-30 12:35:57 +0100

    Added react frontend

--- pm/pm/settings.py
+++ pm/pm/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-
+    'frontend',
     'rest_framework',
     'api',
 ]
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'pm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'static/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
